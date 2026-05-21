a35aa3ab6dfc6c2011632dbea372a8852214e712
Abdulrazaq Salihu <studio.vynix@gmail.com>
2026-05-21 04:38:14 +0100

    build fix

--- build.sh
+++ build.sh
@@ -5,6 +5,16 @@ set -e
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --break-system-packages
 
+# Remove BOM from requirements.txt if present
+python3 -c "
+import sys
+with open('requirements.txt', 'rb') as f:
+    content = f.read()
+if content.startswith(b'\xef\xbb\xbf'):
+    with open('requirements.txt', 'wb') as f:
+        f.write(content[3:])
+"
+
 # Install dependencies with --break-system-packages
 python3 -m pip install --break-system-packages -r requirements.txt
 
