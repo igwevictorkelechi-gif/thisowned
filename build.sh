1cfcbc5e8c2526a6b8554e0f7bb154695fd07227
Abdulrazaq Salihu <studio.vynix@gmail.com>
2026-05-21 04:31:43 +0100

    build(build.sh): simplify to install dependencies
    remove unnecessary pip bootstrapping, static collection and database migration steps from the build script

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 # build.sh
 
 # Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-
-# Install dependencies
+# install dependencies only
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-# Run migrations
-#python manage.py migrate
-
-# Collect static files
-python3 manage.py collectstatic --noinput
