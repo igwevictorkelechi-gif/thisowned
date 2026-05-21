6a93501014cba299d43fd5516f0c3d35088ee1c7
Abdulrazaq Salihu <studio.vynix@gmail.com>
2026-05-21 04:35:55 +0100

    build fix

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-# build.sh
+#!/bin/bash
+set -e
 
-# Install pip
-# install dependencies only
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+# Install pip with --break-system-packages since environment is managed by uv
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --break-system-packages
+
+# Install dependencies with --break-system-packages
+python3 -m pip install --break-system-packages -r requirements.txt
+
+# Run migrations (uncomment if needed)
+# python3 manage.py migrate
+
+# Collect static files
+python3 manage.py collectstatic --noinput
